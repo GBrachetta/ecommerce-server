e2bdaf593dc16ba339233ac8dd3cb1f577584c8c
Guillermo Brachetta <brachetta@me.com>
2021-02-06 00:29:37 +0100

    Protect routes with token permissions

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 from .models import Product
 from .serializers import (
@@ -44,11 +46,22 @@ def getRoutes(request):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     """ summary """
 
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    """ summary """
+
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
