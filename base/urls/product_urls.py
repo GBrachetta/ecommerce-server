871314cad40b67083f93d41c730d81c8687e64a5
Guillermo Brachetta <brachetta@me.com>
2021-02-06 20:39:26 +0100

    Sort imports

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from ..views import product_views as views
 
 urlpatterns = [
--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,8 +1,5 @@
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 
 from ..models import Product
 from ..serializers import ProductSerializer
