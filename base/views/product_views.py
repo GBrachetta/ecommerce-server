e14e62dacd309944da07b5b198b0b6a714cf0d2c
Guillermo Brachetta <brachetta@me.com>
2021-02-11 00:47:28 +0100

    Delete products endpoints

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from ..models import Product
@@ -21,3 +22,13 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    """ summary """
+
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response("Product Deleted")
