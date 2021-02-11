e613a53c7f48c54a9f99760ae25ef088fba737c9
Guillermo Brachetta <brachetta@me.com>
2021-02-11 20:27:41 +0100

    Update error json object for reviews

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -100,11 +100,11 @@ def createProductReview(request, pk):
     alreadyExists = product.review_set.filter(user=user).exists()
 
     if alreadyExists:
-        content = {"details": "Product already reviewed"}
+        content = {"detail": "Product already reviewed"}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
     # 2 - No rating or 0 (must submit rating)
     elif data["rating"] == 0:
-        content = {"details": "Please select a rating"}
+        content = {"detail": "Please select a rating"}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
     # 3 - Create review
     else:
@@ -125,4 +125,4 @@ def createProductReview(request, pk):
             product.rating = total / len(reviews)
         product.save()
 
-        return Response({"details": "Review was added"})
+        return Response({"detail": "Review was added"})
