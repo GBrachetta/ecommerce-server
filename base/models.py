9767fac1cd6d2b4c5d2539014674bda53c3c004b
Guillermo Brachetta <brachetta@me.com>
2021-02-11 10:41:53 +0100

    Create and update product endpoints

--- base/models.py
+++ base/models.py
@@ -1,11 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(
+        null=True, blank=True, default="/placeholder.jpg"
+    )
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -5,5 +5,7 @@ from ..views import product_views as views
 urlpatterns = [
     path("", views.getProducts, name="products"),
     path("<str:pk>", views.getProduct, name="product"),
+    path("create/", views.createProduct, name="product-create"),
+    path("update/<str:pk>/", views.updateProduct, name="product-update"),
     path("delete/<str:pk>/", views.deleteProduct, name="product-delete"),
 ]
