745633b68134d9e4efb815439d983bd4658fcc82
Guillermo Brachetta <brachetta@me.com>
2021-02-12 15:05:59 +0100

    Add build folder

--- backend/settings.py
+++ backend/settings.py
@@ -164,5 +164,5 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "my-proshop"
 AWS_QUERYSTRING_AUTH = False
 
-# if os.getcwd() == "/app":
-#     DEBUG = False
+if os.getcwd() == "/app":
+    DEBUG = False
